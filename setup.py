d972ce28b92b052a99fe01b30d4fc551094bc093
Myles Braithwaite <me@mylesbraithwaite.com>
2022-07-08 22:26:59 -0400

    :wrench: Update the classifiers.

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ import os
 import sys
 from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
+from setuptools import Command, find_packages, setup
 
 # Package meta-data.
-NAME = 'django-jsonfeed'
-DESCRIPTION = 'JSONFeed syndication in Django.'
-URL = 'https://github.com/myles/django-jsonfeed'
-EMAIL = 'me@mylesbraithwaite.org'
-AUTHOR = 'Myles Braithwaite'
+NAME = "django-jsonfeed"
+DESCRIPTION = "JSONFeed syndication in Django."
+URL = "https://github.com/myles/django-jsonfeed"
+EMAIL = "me@mylesbraithwaite.org"
+AUTHOR = "Myles Braithwaite"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -26,25 +26,25 @@ REQUIRED = [
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-with open(os.path.join(here, 'jsonfeed', '__version__.py')) as f:
+with open(os.path.join(here, "jsonfeed", "__version__.py")) as f:
     exec(f.read(), about)
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -54,16 +54,16 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('pipenv run python setup.py sdist bdist_wheel --universal')
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("pipenv run python setup.py sdist bdist_wheel --universal")
 
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('pipenv run twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("pipenv run twine upload dist/*")
 
         sys.exit()
 
@@ -71,31 +71,32 @@ class UploadCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Framework :: Django',
-        'Framework :: Django :: 2.0',
-        'Framework :: Flask',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: MIT License",
+        "Framework :: Django",
+        "Framework :: Django :: 1.0",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 4.0",
+        "Framework :: Flask",
+        "Framework :: Pelican",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
     ],
     # $ setup.py publish support.
     cmdclass={
-        'upload': UploadCommand,
+        "upload": UploadCommand,
     },
 )
